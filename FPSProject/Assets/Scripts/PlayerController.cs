bc84ba71bc9f25407004b690f923b911f3c70a3d
PCNN-K <chanzzang513@gmail.com>
2021-09-09 00:34:24 +0900

    Applying run, jump, sit

--- FPSProject/Assets/Scripts/PlayerController.cs
+++ FPSProject/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // 값을 Inspector 창에서 확인하고 쉽게 수정하기 위해서 다음과 같이 설정
     private float walkSpeed = 3f;
+    private float runSpeed = 6.8f;
+    private float sitSpeed = 1.5f;
+    private float applySpeed;
+
+    private float jumpForce = 5.0f;
+    private float sitPosY = 0f;
+    private float originPosY;
+    private float applySitPosY;
+
+    private bool isRun = false;
+    private bool isGround = true;
+    private bool isSit = false;
 
     private float camSensitivity = 2.4f;
 
@@ -16,13 +27,18 @@ public class PlayerController : MonoBehaviour
     private Camera theCamera;
     private Rigidbody playerRigid;
 
+    private CapsuleCollider playerCollider;
     // Start is called before the first frame update
     void Start()
     {
+        playerCollider = GetComponent<CapsuleCollider>();
         // 게임 오브젝트 중에서 Camera 타입 오브젝트를 찾아서 theCamera에 할당.
         theCamera = FindObjectOfType<Camera>();
         // rigidBody 컴포넌트를 받아와 playerRigid에 할당한다.
         playerRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applySitPosY = originPosY;
     }
 
 
@@ -30,9 +46,115 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TrySit();
         Move();
         CamRotation();
         CharacterRotation();
+    }
+
+    // Ctrl을 눌렀을 시 앉기 수행
+    private void TrySit()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Sit();
+        }
+    }
+
+    // 속도를 앉았을 때 속도로, 위치를 앉았을 때 위치로 변경
+    // 다시 Ctrl 입력이 들어오면 걷는 속도, 원래 위치로 되돌린다.
+    private void Sit()
+    {
+        isSit = !isSit;
+
+        if (isSit)
+        {
+            applySpeed = sitSpeed;
+            applySitPosY = sitPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applySitPosY = originPosY;
+        }
+
+        StartCoroutine(SitCoroutine());
+    }
+
+    // 보간을 이용해서 부드럽게 앉는 과정을 보일 수 있게 한다.
+    IEnumerator SitCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+        while(_posY != applySitPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applySitPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+                break;
+            yield return null;
+        }
+
+        theCamera.transform.localPosition = new Vector3(0, applySitPosY, 0);
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, playerCollider.bounds.extents.y + 0.1f);
+        // 대각선, 계단 같은 곳을 무시할 수 있도록 절반에 0.1f를 더한 길이를 레이저로 쏜다.
+    }
+
+    private void TryJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        // 앉은 상태에서 점프를 시도하면 자동으로 다시 지면에 닿았을 때 그냥 서있도록 한다.
+        if (isSit)
+        {
+            Sit();
+        }
+
+        playerRigid.velocity = transform.up * jumpForce;
+                                        //(0, 1, 0)
+                                        // 공중으로 띄운다.
+    }
+
+    private void TryRun()
+    {
+        // 앉은 상태에서 달리기를 시도하면 무시한다.
+        if (isSit)
+            return;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
     }
 
     private void Move()
@@ -48,7 +170,7 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
         // 움직이는 거리를 1로 조정하기 위해서 normalized를 사용.
 
         playerRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
