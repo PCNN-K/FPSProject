88f0749b0d1740ce14b64fb3001b9001f3918afd
PCNN-K <chanzzang513@gmail.com>
2021-09-14 00:17:23 +0900

    MOD : Upgrade Unity Ver.

---
+++ FPSProject/Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject _poolingAmmoPrefab;
+
+    //ammo의 오브젝트 풀
+    //Hint : 총알 담는 자료구조는 Queue로 구현!
+
+    Queue<Ammo> poolingObjectQueue = new Queue<Ammo>();
+
+    public void Fire()
+    {
+        
+    }
+
+    public void CreateAmmo()
+    {
+
+    }
+
+    private void Shot()
+    {
+
+    }
+
+    private void Awake()
+    {
+        // 오브젝트 풀 초기화 로직
+
+    }
+
+    
+    
+}
