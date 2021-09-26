d3447d2cb185f38d407771094da67649a1f7da11
PCNN-K <chanzzang513@gmail.com>
2021-09-26 22:43:39 +0900

    MOD

--- FPSProject/Assets/Scripts/PlayerManager.Guns.cs
+++ FPSProject/Assets/Scripts/PlayerManager.Guns.cs
@@ -3,6 +3,8 @@ using System.Linq;
 
 public partial class PlayerManager
 {
+    public int selectedIndex = 0;
+
     // 키보드에서 인덱스를 입력받아 해당 인덱스에 총을 반환.
     public Gun SelectGun(int index)
     {
@@ -15,7 +17,6 @@ public partial class PlayerManager
         // IsInvoking이 어떻게 해서 작동하는 지 잘 모르겠음.
         // 현재 Instance화 된 총이 IsInvoking을 만족하는 것인지 아닌지 다른 의미가 있는지 모르겠음.
         // gun 중에서 gun.name을 가진 객체를 찾아서 반환함.
-        _Guns.Where(gun => gun.IsInvoking()).Select(gun => gun.name);
-        return _Guns.First();
+        return SelectGun(selectedIndex);
     }
 }
