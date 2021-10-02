7a844b677dde2641e965752b2c974f8daf19bd67
PCNN-K <chanzzang513@gmail.com>
2021-10-02 09:04:07 +0900

    ADD 오디오 추가

--- FPSProject/Assets/Scripts/Gun.cs
+++ FPSProject/Assets/Scripts/Gun.cs
@@ -8,19 +8,32 @@ public class Gun : MonoBehaviour
     private GameObject poolingAmmoPrefab;
     private ParticleSystem Flash;
     private ParticleSystem FlashParticle;
+    private AudioClip fireSound;
+    private AudioClip fireSoundClip;
+    private AudioSource firesoundsource;
 
     public static Queue<Ammo> AmmoPool = new Queue<Ammo>();
     private Camera mainCamera;
 
-    private int currentAmmo;
-    private int maxAmmo;
-    private int magazineAmmo;
-    private int currentMagazineAmmo;
+    public float fireRate; // 연사 속도
+    public float range; // 사정거리(미구현 상태)
+    public float reloadTime; // 재장전 속도
+    public float currentFireRate;
+
+    public float retroActionForce; // 반동 세기 (미구현 상태)
+    public float retroActionFineSightForce; // 정조준 시 반동 세기
+
+    public int carryAmmo; // 현재 들고 있는 총알의 개수
+    public int maxAmmo; // 최대로 소유할 수 있는 총알의 개수
+    public int magazineAmmo; // 한 탄알집에 들어가는 총알의 개수
+    public int currentMagazineAmmo; // 현재 탄알집에 들어가 있는 총알의 개수
+
+    public Vector3 fineSightOriginPosition; // 정조준시 총의 위치
 
     private void Awake()
     {
         magazineAmmo = 30;
-        currentAmmo = magazineAmmo;
+        carryAmmo = magazineAmmo * 5;
         currentMagazineAmmo = magazineAmmo;
         maxAmmo = 200;
         //초기화 로직
@@ -29,9 +42,14 @@ public class Gun : MonoBehaviour
 
     private void Start()
     {
+        reloadTime = 2.0f;
+        fireRate = 0.3f;
         mainCamera = FindObjectOfType<Camera>();
         Flash = Resources.Load<ParticleSystem>("Prefabs/Particles/Flash");
         FlashParticle = Instantiate(Flash, transform.Find("BulletPos").position, transform.Find("BulletPos").rotation);
+        //fireSound = Resources.Load<AudioClip>("Audios/fire_sound");
+        //fireSoundClip = Instantiate(fireSound, transform);
+
     }
 
     public int GetCurrentMagazineAmmo
@@ -52,16 +70,31 @@ public class Gun : MonoBehaviour
 
     public void Fire(Vector3 _target)
     {
-        var ammo = GetObject();
-        ammo.transform.position = transform.Find("BulletPos").position;
-        ammo.transform.rotation = transform.Find("BulletPos").rotation;
+        if(currentFireRate <= 0)
+        {
+            currentFireRate = fireRate;
+            var ammo = GetObject();
+            ammo.transform.position = transform.Find("BulletPos").position;
+            ammo.transform.rotation = transform.Find("BulletPos").rotation;
 
-        FlashParticle.Play();
-        ammo.Shoot(_target);
+            FlashParticle.Play();
+            //PlaySE(fireSoundClip);
+            ammo.Shoot(_target);
+        }
+        
+    }
+
+    private void FireRateCalc()
+    {
+        if(currentFireRate > 0)
+        {
+            currentFireRate -= Time.deltaTime;
+        }
     }
 
     private void Update()
     {
+        FireRateCalc();
     }
 
     // 오브젝트 풀에 새로운 오브젝트를 일정 개수만큼 생성해서 Enqueue
@@ -112,4 +145,9 @@ public class Gun : MonoBehaviour
         AmmoPool.Enqueue(_obj);
     }
 
+    private void PlaySE(AudioClip _clip)
+    {
+        firesoundsource.clip = _clip;
+        firesoundsource.Play();
+    }
 }
